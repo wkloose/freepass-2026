29b0b983f0381c948f10ecccee3e0b01a9042246
wkloose <hisyamalif13@gmail.com>
2026-02-04 14:34:54 +0700

    feat(repo): implement user repository with dependency injection

---
+++ repositories/user_repository.go
@@ -0,0 +1,43 @@
+package repositories
+
+import (
+	"github.com/Hisyam/freepass-2026/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+	FindByEmail(email string) (*models.User, error)
+	FindByID(id uuid.UUID) (*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db}
+}
+
+func (r *userRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) FindByID(id uuid.UUID) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
